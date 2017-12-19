e47c21b10e3c9a51509761986a86da7faa94fe3f
tut173313 <r173313@edu.tut.ac.jp>
2017-12-19 13:54:58 +0900

    2nd week first commit

--- s4/b173313/TestCase.java
+++ s4/b173313/TestCase.java
@@ -52,18 +52,27 @@ public class TestCase {
 	    System.out.println("checking s4.b173313.InformationEstimator");
 	    myObject = new s4.b173313.InformationEstimator();
 	    myObject.setSpace("3210321001230123".getBytes());
-	    myObject.setTarget("0".getBytes());
+	    
+		myObject.setTarget("0".getBytes());
 	    value = myObject.estimation();
 	    System.out.println(">0 "+value);
-	    myObject.setTarget("01".getBytes());
+	    
+		myObject.setTarget("01".getBytes());
 	    value = myObject.estimation();
 	    System.out.println(">01 "+value);
-	    myObject.setTarget("0123".getBytes());
+	    
+		myObject.setTarget("0123".getBytes());
 	    value = myObject.estimation();
 	    System.out.println(">0123 "+value);
-	    myObject.setTarget("00".getBytes());
+	    
+		myObject.setTarget("00".getBytes());
 	    value = myObject.estimation();
 	    System.out.println(">00 "+value);
+		
+		//---------------------------------------
+		myObject.setTarget(" ".getBytes());
+	    value = myObject.estimation();
+	    System.out.println(">_ "+value);
 	}
 	catch(Exception e) {
 	    System.out.println("Exception occurred: STOP");
